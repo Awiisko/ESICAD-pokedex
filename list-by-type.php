67a4c28c62c3cfa72ef808037d0e77f73df0beb4
Awiisko <leo.dibattitsta31770@gmail.com>
2024-03-04 10:21:29 +0100

    push listetype/details

--- list-by-type.php
+++ list-by-type.php
@@ -4,14 +4,33 @@
 <?php
 require_once("head.php");
 require_once("database-connection.php");
-$query = $databaseconnection->query
-?>
-<pre>
-    &lt;
-    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS CLASSES PAR LEUR TYPE, PUIS PAR LEUR NOM.
-    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES
-    &gt;
-    </pre>
-<?php
+
+$queryTypes = $databaseConnection->query("SELECT DISTINCT libelleType FROM typepokemon ORDER BY libelleType");
+
+if (!$queryTypes) {
+    echo "Erreur SQL : " . $databaseConnection->error;
+} else {
+    while ($type = $queryTypes->fetch_assoc()) {
+        echo '<h2>' . $type['libelleType'] . '</h2>';
+        echo '<table>';
+
+        $queryPokemon = $databaseConnection->query("SELECT pokemon.IdPokemon, pokemon.NomPokemon, pokemon.urlPhoto FROM pokemon INNER JOIN typepokemon ON pokemon.IdTypePokemon = typepokemon.IdType WHERE typepokemon.libelleType = '" . $type['libelleType'] . "' ORDER BY pokemon.IdPokemon");
+
+        if (!$queryPokemon) {
+            echo "Erreur SQL : " . $databaseConnection->error;
+        } else {
+            while ($pokemon = $queryPokemon->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $pokemon['IdPokemon'] . '</td>';
+                echo '<td><a href="pokemon_details.php?id=' . $pokemon['IdPokemon'] . '"><img src="' . $pokemon['urlPhoto'] . '" alt="' . $pokemon['NomPokemon'] . '"></a></td>';
+                echo '<td><a href="pokemon_details.php?id=' . $pokemon['IdPokemon'] . '">' . $pokemon['NomPokemon'] . '</a></td>';
+                echo '</tr>';
+            }
+        }
+
+        echo '</table>';
+    }
+}
+
 require_once("footer.php");
 ?>
