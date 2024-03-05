979e68e7902e1a2a4c9515a85f42b5e154aff403
Awiisko <leo.dibattitsta31770@gmail.com>
2024-03-05 12:59:08 +0100

    avancée site

--- list.php
+++ list.php
@@ -4,15 +4,14 @@
 <?php
 require_once("head.php");
 ?>
-    <table class = "tableau_pokemon">
-    <thead class = "tableau_all">
+    <table class="tableau_pokemon">
+    <thead class="tableau_all">
         <th>N°</th>
         <th>Nom</th>
         <th>Photo</th>
         <th>1er Type</th>
         <th>2ème Type</th>
         <?php
-require_once("head.php");
 require_once("database-connection.php");
 
 $query = $databaseConnection->query("SELECT pokemon.IdPokemon, pokemon.NomPokemon, pokemon.urlPhoto, typepokemon.libelleType, typepokemon2.libelleType AS 'Type 2' FROM pokemon LEFT JOIN typepokemon ON pokemon.IdTypePokemon = typepokemon.IdType LEFT JOIN typepokemon as typepokemon2 ON pokemon.idSecondTypePokemon = typepokemon2.IdType ORDER BY pokemon.IdPokemon");
